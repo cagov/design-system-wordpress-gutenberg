ba426c9df20974115ec6f7f2b8e11ec60488dde8
Danny Guzman <guzmanjd86@gmail.com>
2022-05-06 17:44:47 -0700

    Built out ds-link-grid-card a child block that goes inside of the ds-link-grid block

---
+++ blocks/ds-link-grid-card/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'a23331a20da3f644be1b706ebe264c43');
