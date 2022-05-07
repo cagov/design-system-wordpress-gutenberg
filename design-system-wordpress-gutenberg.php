755facaaa1a713aea836790b53d3ec59fc21e081
Danny Guzman <guzmanjd86@gmail.com>
2022-05-06 17:27:49 -0700

    Added WordPress Coding Standards composer package Added gulp-file and cross-env npm packages Fixed small script issues with gulp tasks, files are now built both minified and unminified Enforced WordPress Coding Standards on main plugin file, removed development commented out code Moved actions/filters to top of the file for easier visibility Renamed function suffixes to match the actions/filters they are attached too Simpliefied the allowed blocks filter

--- design-system-wordpress-gutenberg.php
+++ design-system-wordpress-gutenberg.php
@@ -1,16 +1,18 @@
 <?php
 /**
- * Plugin Name: ca.gov Design System Gutenberg blocks extension plugin
+ * Plugin Name: ca.gov Design System Gutenberg Blocks
+ *
  * @source https://github.com/cagov/design-system/
  * Plugin URI: https://github.com/cagov/design-system-wordpress-gutenberg
- * Description: Integrate the <a href="https://designsystem.webstandards.ca.gov">State of California Design System</a> into the WordPress Gutenberg editor.
+ * Description: Integrates the <a href="https://designsystem.webstandards.ca.gov">State of California Design System</a> into the WordPress.
  * Author: Office of Digital Innovation
  * Author URI: https://digital.ca.gov
  * Version: 1.2.0
  * License: MIT
  * License URI: https://opensource.org/licenses/MIT
  * Text Domain: cagov-design-system
- *
+ * Requires at least: 5.8
+ * 
  * @package  cagov-design-system
  * @author   Office of Digital Innovation <info@digital.ca.gov>
  * @license  https://opensource.org/licenses/MIT MIT
@@ -19,165 +21,171 @@
  * Domain Path: /languages
  */
 
-if (!defined('ABSPATH')) {
+if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
 // Plugin constants.
+$cagov_doc_root = isset( $_SERVER['DOCUMENT_ROOT'] ) ? sanitize_text_field( wp_unslash( $_SERVER['DOCUMENT_ROOT'] ) ) : '';
+
 define( 'CAGOV_DESIGN_SYSTEM_GUTENBERG', __DIR__ );
 define( 'CAGOV_DESIGN_SYSTEM_GUTENBERG__VERSION', '1.2.0.3' );
-define( 'CAGOV_DESIGN_SYSTEM_GUTENBERG_URI', esc_url(str_replace($_SERVER['DOCUMENT_ROOT'], '', __DIR__)) );
-define( 'CAGOV_DESIGN_SYSTEM_GUTENBERG__DEBUG', true ); // Can associate with env variable later
-
-// define( 'CAGOV_DESIGN_SYSTEM_BUNDLE', "https://cdn.designsystem.webstandards.ca.gov/bundles/v1.0.0/cagov-design-system.min.js" ); // Bundle 
-// define( 'CAGOV_DESIGN_SYSTEM_BUNDLE', "https://cdn.designsystem.webstandards.ca.gov/bundles/v1.0.0/cagov-design-system.main.js" ); // Bundle 
-// define( 'CAGOV_DESIGN_SYSTEM_BUNDLE', "https://cdn.designsystem.webstandards.ca.gov/bundles/v1.0.0/cagov-design-system.staging.js" ); // Bundle 
-define( 'CAGOV_DESIGN_SYSTEM_BUNDLE', "https://cdn.designsystem.webstandards.ca.gov/bundles/v1.0.0/cagov-design-system.development.js" ); // Bundle instructions
-define( 'CAGOV_DESIGN_SYSTEM_BUNDLE_LOCAL', CAGOV_DESIGN_SYSTEM_GUTENBERG_URI . "/build/js/cagov-design-system.core.js"); // Bundle instructions
-
-add_action( 'init', 'cagov_ds_gutenberg_enqueue_block_editor_assets' );
-// add_action( 'wp_enqueue_scripts', 'cagov_ds_gutenberg_wp_enqueue_scripts', 99999 ); // Not needed yet but very likely will be.
-
+define( 'CAGOV_DESIGN_SYSTEM_GUTENBERG_URI', esc_url( str_replace( $cagov_doc_root, '', __DIR__ ) ) );
+define( 'CAGOV_DESIGN_SYSTEM_GUTENBERG__DEBUG', true ); // Can associate with env variable later.
 
 /**
- * Load custom styles
+ * Plugin API/Action Reference
+ * Actions Run During a Typical Request
+ *
+ * @link https://codex.wordpress.org/Plugin_API/Action_Reference#Actions_Run_During_a_Typical_Request
  */
-function cagov_ds_gutenberg_wp_enqueue_scripts(){
-	// wp_enqueue_style( 'cagov-design-system-style', $core_css, array(), CAGOV_DESIGN_SYSTEM_BUNDLE ); // For debugging, Design System bundle has no CSS, but can use this for debugging when there is a bug in the Design System.
-}
+global $wp_version;
 
-function cagov_ds_gutenberg_enqueue_header_scripts()
-{
-	$handle  = 'cagov-design-system-components-script';
-	$src     = CAGOV_DESIGN_SYSTEM_BUNDLE_LOCAL;
-	$deps    = [];
-	$version = CAGOV_DESIGN_SYSTEM_GUTENBERG__VERSION;
-	wp_enqueue_script( $handle, $src, $deps, $version, true );
-}
+$cagov_ds_is_under_5_8 = version_compare( $wp_version, '5.8', '<' ) ? '' : '_all';
 
-function cagov_ds_gutenberg_add_type_attribute($tag, $handle, $src) {
-    // Register script as module
-    if ( 'cagov-design-system-components-script' !== $handle ) {
-        return $tag;
-    }
-    $tag = '<script type="module" src="' . esc_url( $src ) . '"></script>';
-    
-    return $tag;
-}
+add_action( 'init', 'cagov_ds_gutenberg_enqueue_block_editor_assets' );
+
+add_filter( "block_categories$cagov_ds_is_under_5_8", 'cagov_ds_gutenberg_categories', 10, 2 );
+add_filter( "allowed_block_types$cagov_ds_is_under_5_8", 'cagov_ds_gutenberg_allowed_block_types' );
+
+add_filter( 'script_loader_tag', 'cagov_ds_gutenberg_script_loader_tag', 10, 3 );
+
 
 
 /**
  * Include Gutenberg Block assets by getting the index file of each block build file.
+ *
+ * @return void
  */
-function cagov_ds_gutenberg_enqueue_block_editor_assets(){
-    global $wp_version;
+function cagov_ds_gutenberg_enqueue_block_editor_assets() {
 
-    $is_under_5_8 = version_compare($wp_version, "5.8", '<') ? '' : '_all';
-
-    add_filter("block_categories$is_under_5_8", 'cagov_ds_gutenberg_categories', 10, 2);
-
-
-    add_action('admin_enqueue_scripts', 'cagov_ds_gutenberg_enqueue_header_scripts', 10);
-    add_action('wp_enqueue_scripts', 'cagov_ds_gutenberg_enqueue_header_scripts', 10);
-    add_filter('script_loader_tag', 'cagov_ds_gutenberg_add_type_attribute' , 10, 3);
-
-    add_filter( 'allowed_block_types', 'cagov_ds_gutenberg_allowed_block_types' );
-    
 	// Register shared packages.
 	// @TODO check performance after a few components are re-mapped
-    $deps = array(
-        // 'jquery', (DEPRECATING: Let's not use jQuery with React.)
-        'wp-blocks', 
-        'wp-element', 
-        'wp-editor',
-        'wp-i18n',
-        'wp-block-editor',
-        'wp-rich-text'
-    );
+	// 'jquery', (DEPRECATING: Let's not use jQuery with React.).
+	$deps = array(
+		'wp-blocks',
+		'wp-element',
+		'wp-editor',
+		'wp-i18n',
+		'wp-block-editor',
+		'wp-rich-text',
+		'wp-components',
+	);
 
 	// Register compiled Gutenberg Block bundles.
-    if (false === CAGOV_DESIGN_SYSTEM_GUTENBERG__DEBUG) {
-        wp_register_script( 'cagov-design-system-gutenberg', cagov_ds_gutenberg_get_min_file( '/build/js/gutenberg.js', 'js' ), $deps, CAGOV_DESIGN_SYSTEM_GUTENBERG__VERSION, true );
-        wp_register_style( 'cagov-design-system-gutenberg', cagov_ds_gutenberg_get_min_file( '/build/css/gutenberg.css' ), array(), CAGOV_DESIGN_SYSTEM_GUTENBERG__VERSION );
-        wp_register_style( 'cagov-design-system-gutenberg-style', cagov_ds_gutenberg_get_min_file( '/build/css/cagov-design-system.css' ), array(), CAGOV_DESIGN_SYSTEM_GUTENBERG__VERSION );
-    
-        // Register all CA Design System Gutenberg Blocks.
-        foreach( glob(CAGOV_DESIGN_SYSTEM_GUTENBERG . '/blocks/*/') as $block ){
-            $name = basename($block);
-            register_block_type(strtolower(CAGOV_DESIGN_SYSTEM_GUTENBERG . "/blocks/$name/build"));
-        }
-    } else {
-        // Load `src` code from Gutenberg blocks editor. 
-        wp_register_script( 'cagov-design-system-gutenberg', cagov_ds_gutenberg_get_min_file( '/build/js/gutenberg.debug.js', 'js' ), $deps, CAGOV_DESIGN_SYSTEM_GUTENBERG__VERSION, true );
-        wp_register_style( 'cagov-design-system-gutenberg', cagov_ds_gutenberg_get_min_file( '/build/css/gutenberg.debug.css' ), array(), CAGOV_DESIGN_SYSTEM_GUTENBERG__VERSION );
-        wp_register_style( 'cagov-design-system-gutenberg-style', cagov_ds_gutenberg_get_min_file( '/build/css/cagov-design-system.debug.css' ), array(), CAGOV_DESIGN_SYSTEM_GUTENBERG__VERSION );
-    
-        // Register all CA Design System Gutenberg Blocks.
-        foreach( glob(CAGOV_DESIGN_SYSTEM_GUTENBERG . '/blocks/*/') as $block ){
-            $name = basename($block);
-            register_block_type(strtolower(CAGOV_DESIGN_SYSTEM_GUTENBERG . "/blocks/$name/src"));
-        }
-    }
+	wp_register_script( 'cagov-design-system-gutenberg', cagov_ds_gutenberg_get_min_file( '/build/js/gutenberg.js', 'js' ), $deps, CAGOV_DESIGN_SYSTEM_GUTENBERG__VERSION, true );
+	wp_register_script( 'cagov-design-system-components-script', cagov_ds_gutenberg_get_min_file( '/build/js/cagov-design-system.js' ), array(), CAGOV_DESIGN_SYSTEM_GUTENBERG__VERSION, true );
+
+	wp_register_style( 'cagov-design-system-gutenberg', cagov_ds_gutenberg_get_min_file( '/build/css/gutenberg.css' ), array(), CAGOV_DESIGN_SYSTEM_GUTENBERG__VERSION );
+	wp_register_style( 'cagov-design-system-gutenberg-style', cagov_ds_gutenberg_get_min_file( '/build/css/cagov-design-system.css' ), array(), CAGOV_DESIGN_SYSTEM_GUTENBERG__VERSION );
+
+	// Register all CA Design System Gutenberg Blocks.
+	foreach ( glob( CAGOV_DESIGN_SYSTEM_GUTENBERG . '/blocks/*/' ) as $block ) {
+		$name = basename( $block );
+		register_block_type( strtolower( CAGOV_DESIGN_SYSTEM_GUTENBERG . "/blocks/$name/build" ) );
+	}
+
 }
 
 /**
  * Register Gutenberg Blocks categories to the Block editor
+ *
+ * @link https://developer.wordpress.org/reference/hooks/block_categories_all/
+ *
+ * @param  array                   $categories Array of categories for block types.
+ * @param  WP_Block_Editor_Context $post The current block editor context.
+ * @return array
  */
-function cagov_ds_gutenberg_categories($categories, $post) {
-    return array_merge(
-        array(
-            array(
-                'slug'  => 'cagov-design-system',
-                'title' => 'CA Design System',
-            ),
-        ),
-        array(
-            array(
-                'slug'  => 'cagov-design-system-utilities',
-                'title' => 'CAGov Design System: Utilities',
-            ),
-        ),
-        $categories,
-    );
+function cagov_ds_gutenberg_categories( $categories, $post ) {
+	return array_merge(
+		array(
+			array(
+				'slug'  => 'cagov-design-system',
+				'title' => 'CA Design System',
+			),
+		),
+		array(
+			array(
+				'slug'  => 'cagov-design-system-utilities',
+				'title' => 'CAGov Design System: Utilities',
+			),
+		),
+		$categories
+	);
 }
 
 /**
  * Load Minified Version of a file
  *
  * @param  string $f File to load.
- * @param  mixed  $ext Extension of file, default css.
+ * @param  string $ext Extension of file, default css.
  *
  * @return string
  */
 function cagov_ds_gutenberg_get_min_file( $f, $ext = 'css' ) {
-	/* if a minified version exists load it */
-	if ( file_exists( CAGOV_DESIGN_SYSTEM_GUTENBERG . str_replace( ".$ext", ".min.$ext", $f ) ) ) {
+	// if not debugging and a minified version exists load it.
+	if ( CAGOV_DESIGN_SYSTEM_GUTENBERG__DEBUG && file_exists( CAGOV_DESIGN_SYSTEM_GUTENBERG . str_replace( ".$ext", ".min.$ext", $f ) ) ) {
 		return CAGOV_DESIGN_SYSTEM_GUTENBERG_URI . str_replace( ".$ext", ".min.$ext", $f );
 	} else {
 		return CAGOV_DESIGN_SYSTEM_GUTENBERG_URI . $f;
 	}
 }
 
-/** If we want to remove any blocks or patterns from Gutenberg, this is how we can do it something like this. */
-function cagov_ds_gutenberg_allowed_block_types($allowed_blocks)
-{
-    remove_theme_support('core-block-patterns');
-    // Return the desired components
-    return array(
-        'core/image',
-        'core/paragraph',
-        'core/button',
-        'core/table',
-        'core/heading',
-        'core/list',
-        'core/custom-html',
-        'core/classic',
-        'cagov/ds-accordion',
-        // 'cagov/card', // Missing but necessary.
-        'cagov/ds-link-grid',
-        'cagov/ds-feature-card',
-        'cagov/ds-page-alert',
-        'cagov/ds-regulatory-outline',
-        'cagov/ds-step-list',
-    );
+/**
+ * Design System Allowed Block Types
+ *
+ * Removes all blocks or patterns from Gutenberg and returns Design System Blocks.
+ *
+ * @link https://developer.wordpress.org/reference/hooks/allowed_block_types_all/
+ *
+ * @param  bool|array $allowed_blocks Array of block type slugs, or boolean to enable/disable all. Default true (all registered block types supported).
+ * @return array
+ */
+function cagov_ds_gutenberg_allowed_block_types( $allowed_blocks ) {
+
+	// if not debugging, return all blocks.
+	if ( ! CAGOV_DESIGN_SYSTEM_GUTENBERG__DEBUG ) {
+		return $allowed_blocks;
+	}
+
+	remove_theme_support( 'core-block-patterns' );
+
+	// Core Components
+	$core = array(
+		'core/image',
+		'core/paragraph',
+		'core/button',
+		'core/table',
+		'core/heading',
+		'core/list',
+		'core/custom-html',
+		'core/classic',
+	);
+
+	// Dynamically get a list of the Design System blocks
+	$design_system = array_map( function($b){
+		return 'cagov-design-system/' . basename($b);
+	},
+	glob( CAGOV_DESIGN_SYSTEM_GUTENBERG . '/blocks/*')
+	);
+
+	// Return the desired components.
+	return array_merge($core, $design_system );
+}
+
+/**
+ * Filters the HTML script tag of an enqueued script.
+ *
+ * @param  mixed $tag The <script> tag for the enqueued script.
+ * @param  mixed $handle The script's registered handle.
+ * @param  mixed $src The script's source URL.
+ * @return string
+ */
+function cagov_ds_gutenberg_script_loader_tag( $tag, $handle, $src ) {
+	// Register script as module.
+	if ( 'cagov-design-system-components-script' === $handle ) {
+		$tag = str_replace( 'src', 'type="module" src', $tag );
+	}
+
+	return $tag;
 }
--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,9 @@
 // CSS related plugins.
 const sass = require('gulp-sass')(require('node-sass')); // Gulp plugin for Sass compilation.
 
+// JS related plugins.
+const uglify = require('gulp-uglify-es').default; // Minifies JS files.
+
 // Utility related plugins.
 const del = require('del'); // Delete plugin
 const lineec = require('gulp-line-ending-corrector'); // Consistent Line Endings for non UNIX systems. Gulp Plugin for Line Ending Corrector (A utility that makes sure your files have consistent line endings).
@@ -24,39 +27,34 @@ var tap = require('gulp-tap');
 var log = require('fancy-log');
 var path = require('path');
 const fs = require('fs'); // File System
-var glob = require("glob")
+const newFile = require('gulp-file'); // File System
+var argv = require('yargs').argv;
 
-const isNotFile = fileName => {
-	return ! fs.lstatSync(fileName).isFile()
-}
+/**
+ * Task to run tests with
+ */
+task('test', async function(){
+	//console.log('info');
+})
 
 /**
  * Task to build all CAGov Design System CSS/JS
  */
 task('build', async function(){
-	var blocks = fs.readdirSync('./blocks/').map(fileName => {
-			return path.join('./blocks/', fileName)
-		}).filter(isNotFile);
+	// build unminified files.
+	buildGutenberEditorCSS(false);
+	buildGutenberEditorJS(false);
+	buildDesignSystemCSS(false);
+	buildDesignSystemJS(false);
 
-	if ( !blocks.length ){
-		fs.writeFileSync('build/css/gutenberg.css', '');
-		fs.writeFileSync('build/css/cagov-design-system.css', '');
-		fs.writeFileSync('build/js/gutenberg.js', '');
-		fs.writeFileSync('build/js/cagov-design-system.js', '');
-	}else{
-		parallel(
-			buildGutenberEditorCSS,
-			buildGutenberEditorJS,
-			buildDesignSystemCSS,
-			buildDesignSystemJS,
-			buildGutenberEditorCSSDebug, // Can split these out
-			buildGutenberEditorJSDebug,
-			buildDesignSystemCSSDebug,
-			buildDesignSystemJSDebug	
-		)();
-	}
-	
+	// build minified files.
+	buildGutenberEditorCSS();
+	//buildGutenberEditorJS();
+	buildDesignSystemCSS();
+	buildDesignSystemJS();
+
 });
+
 
 /**
  * Task to build all CAGov Design System Component Blocks
@@ -64,12 +62,36 @@ task('build', async function(){
 task('build-blocks', buildGutenbergBlocks );
 
 /**
+ * Task to build individual block
+ */
+task('build-block', async function(){
+	if( '%npm_config_name%' == argv.name || undefined == argv.name ){
+		return;
+	}
+
+	src(['./blocks/' + argv.name + '/'])
+        .pipe(tap (function(file){
+			try {
+				shell.task("cd " + file.path + " && npm run build")()
+			} catch (error) {
+				// Note - error messages will vary depending on browser
+				console.error(file.path + ' failed to compile');
+			}	
+        }))
+});
+
+/**
  * Runs Gutenberg Block build script 
  */
 async function buildGutenbergBlocks(){
     src(['./blocks/*/'])
         .pipe(tap (function(file){
-			shell.task("cd " + file.path + " && npm run build")()
+			try {
+				shell.task("cd " + file.path + " && npm run build")()
+			} catch (error) {
+				// Note - error messages will vary depending on browser
+				console.error(file.path + ' failed to compile');
+			}
         }))
 
 }
@@ -77,36 +99,86 @@ async function buildGutenbergBlocks(){
 /**
  * Build Gutenberg Block Editor CSS file
  */
-async function buildGutenberEditorCSS(){
-	del(['build/css/gutenberg.css']);
+async function buildGutenberEditorCSS(min = true){
+	var buildOutputStyle = min ? 'compressed' : 'expanded';
+	var minified = min ? '.min' : '';
+	var t = min ? ' Minified ' : '';
+	var fileName = 'gutenberg' + minified + '.css';
 
-    // Gutenberg Block Editor CSS
-    if (gutenbergEditorCSS.length){
-		src(gutenbergEditorCSS.concat(designSystemCSS))
-			.pipe(lineec()) // Consistent Line Endings for non UNIX systems.
-			.pipe(concat('gutenberg.css')) // compiled file
-			.pipe(dest('build/css/'))
-			.pipe(tap(function (file) {
-				log('[ ✅ Gutenberg Block Editor CSS ] ' + path.basename(file.path) + ' was created successfully.');
-			}))
+	try {
+		del(['build/css/' + fileName]);
+
+		// Gutenberg Block Editor CSS
+		if (gutenbergEditorCSS.length){
+			src(gutenbergEditorCSS.concat(designSystemCSS))
+				.pipe(
+					sass({
+						outputStyle: buildOutputStyle,
+					})
+				)
+				.pipe(lineec()) // Consistent Line Endings for non UNIX systems.
+				.pipe(concat(fileName)) // compiled file
+				.pipe(dest('build/css/'))
+				.pipe(tap(function (file) {
+					log('[ ✅ Gutenberg Block Editor ' + t + 'CSS ] ' + path.basename(file.path) + ' was created successfully.');
+				}))
+		}
+	
+		fs.access('build/css/' + fileName, fs.F_OK, (err) => {
+			if (err) {
+				newFile(fileName, '')
+			        .pipe(dest('build/css/'));
+				return
+			}
+		  });
+			
+	} catch (error) {
+		// Note - error messages will vary depending on browser
+		console.error(fileName + ' failed to compile');
 	}
+
+
 }
 
 /**
  * Build Gutenberg Block Editor JS file
  */
-async function buildGutenberEditorJS(){
-	del(['build/js/gutenberg.js']);
+async function buildGutenberEditorJS(min = true){
+	var minified = min ? '.min' : '';
+	var t = min ? ' Minified ' : '';
+	var fileName = 'gutenberg' + minified + '.js';
 	
-    // Gutenberg Block Editor JS
-	if (gutenbergEditorJS.length){
-		src(gutenbergEditorJS)
-		.pipe(lineec()) // Consistent Line Endings for non UNIX systems.
-		.pipe(concat('gutenberg.js')) // compiled file
-		.pipe(dest('build/js/'))
-		.pipe(tap(function (file) {
-			log('[ ✅ Gutenberg Block Editor JS ] ' + path.basename(file.path) + ' was created successfully.');
-		}))
+	try {
+		del(['build/js/' + fileName]);
+
+		// Gutenberg Block Editor JS
+		if (gutenbergEditorJS.length){
+			let js = src(gutenbergEditorJS)
+			.pipe(lineec()) // Consistent Line Endings for non UNIX systems.
+			.pipe(concat(fileName)) // compiled file
+			.pipe(tap(function (file) {
+				log('[ ✅ Gutenberg Block Editor ' + t + 'JS ] ' + path.basename(file.path) + ' was created successfully.');
+			}));
+	
+			if (min) {
+				js = js.pipe(uglify());
+			}
+	
+			js.pipe(dest('build/js/'))
+	
+		}
+	
+		fs.access('build/js/' + fileName, fs.F_OK, (err) => {
+			if (err) {
+				newFile(fileName, '')
+			        .pipe(dest('build/js/'));
+				return
+			}
+		  });
+			
+	} catch (error) {
+		// Note - error messages will vary depending on browser
+		console.error(fileName + ' failed to compile');
 	}
 
 }
@@ -114,121 +186,84 @@ async function buildGutenberEditorJS(){
 /**
  * Build Design System CSS file
  */
-async function buildDesignSystemCSS(){
-	del(['build/css/cagov-design-system.css']);
+async function buildDesignSystemCSS(min = true){
+	var buildOutputStyle = min ? 'compressed' : 'expanded';
+	var minified = min ? '.min' : '';
+	var t = min ? ' Minified ' : '';
+	var fileName = 'cagov-design-system' + minified + '.css';
 
-	// Design System Front End CSS
-	if (designSystemCSS.length){
-		src(designSystemCSS)
-		.pipe(
-			sass({
-				outputStyle: 'expanded',
-			})
-		)
-		.pipe(lineec()) // Consistent Line Endings for non UNIX systems.
-		.pipe(concat('cagov-design-system.css')) // compiled file
-		.pipe(dest('build/css/'))
-		.pipe(tap(function (file) {
-			log('[ ✅ Design System Frontend CSS ] ' + path.basename(file.path) + ' was created successfully.');
-		}))
-	}
-
-}
-
-/**
- * Build Design System JS file
- */
-async function buildDesignSystemJS(){
-	del(['build/js/cagov-design-system.js']);
-
-	// Design System Front End JS
-	if (designSystemJS.length){
-		src(designSystemJS)
-		.pipe(lineec()) // Consistent Line Endings for non UNIX systems.
-		.pipe(concat('cagov-design-system.js')) // compiled file
-		.pipe(dest('build/js/'))
-		.pipe(tap(function (file) {
-			log('[ ✅ Design System Frontend JS ] ' + path.basename(file.path) + ' was created successfully.');
-		}))
-	}
-}
-
-
-/**
- * Build Gutenberg Block Editor CSS file
- */
- async function buildGutenberEditorCSSDebug(){
-	del(['build/css/gutenberg.css']);
-
-    // Gutenberg Block Editor CSS
-    if (gutenbergEditorCSS.length){
-		src(gutenbergEditorCSS.concat(designSystemCSS))
+	try {
+		del(['build/css/' + fileName]);
+	
+		// Design System Front End CSS
+		if (designSystemCSS.length){
+			src(designSystemCSS)
+			.pipe(
+				sass({
+					outputStyle: buildOutputStyle,
+				})
+			)
 			.pipe(lineec()) // Consistent Line Endings for non UNIX systems.
-			.pipe(concat('gutenberg.debug.css')) // compiled file
+			.pipe(concat(fileName)) // compiled file
 			.pipe(dest('build/css/'))
 			.pipe(tap(function (file) {
-				log('[ ✅ Gutenberg Block Editor CSS ] ' + path.basename(file.path) + ' was created successfully.');
+				log('[ ✅ Design System Frontend ' + t + 'CSS ] ' + path.basename(file.path) + ' was created successfully.');
 			}))
-	}
-}
-
-/**
- * Build Gutenberg Block Editor JS file
- */
-async function buildGutenberEditorJSDebug(){
-	del(['build/js/gutenberg.js']);
+		}
 	
-    // Gutenberg Block Editor JS
-	if (gutenbergEditorJS.length){
-		src(gutenbergEditorJS)
-		.pipe(lineec()) // Consistent Line Endings for non UNIX systems.
-		.pipe(concat('gutenberg.debug.js')) // compiled file
-		.pipe(dest('build/js/'))
-		.pipe(tap(function (file) {
-			log('[ ✅ Gutenberg Block Editor JS ] ' + path.basename(file.path) + ' was created successfully.');
-		}))
+		fs.access('build/css/' + fileName, fs.F_OK, (err) => {
+			if (err) {
+				newFile(fileName, '')
+			        .pipe(dest('build/css/'));
+				return
+			}
+		  })
+			
+	} catch (error) {
+		// Note - error messages will vary depending on browser
+		console.error(fileName + ' failed to compile');
 	}
 
-}
-
-/**
- * Build Design System CSS file
- */
-async function buildDesignSystemCSSDebug(){
-	del(['build/css/cagov-design-system.css']);
-
-	// Design System Front End CSS
-	if (designSystemCSS.length){
-		src(designSystemCSS)
-		.pipe(
-			sass({
-				outputStyle: 'expanded',
-			})
-		)
-		.pipe(lineec()) // Consistent Line Endings for non UNIX systems.
-		.pipe(concat('cagov-design-system.debug.css')) // compiled file
-		.pipe(dest('build/css/'))
-		.pipe(tap(function (file) {
-			log('[ ✅ Design System Frontend CSS ] ' + path.basename(file.path) + ' was created successfully.');
-		}))
-	}
 
 }
 
 /**
  * Build Design System JS file
  */
-async function buildDesignSystemJSDebug(){
-	del(['build/js/cagov-design-system.js']);
+async function buildDesignSystemJS(min = true){
+	var minified = min ? '.min' : '';
+	var t = min ? ' Minified ' : '';
+	var fileName = 'cagov-design-system' + minified + '.js';
+	
+	try {
+		del(['build/js/' + fileName]);
 
-	// Design System Front End JS
-	if (designSystemJS.length){
-		src(designSystemJS)
-		.pipe(lineec()) // Consistent Line Endings for non UNIX systems.
-		.pipe(concat('cagov-design-system.debug.js')) // compiled file
-		.pipe(dest('build/js/'))
-		.pipe(tap(function (file) {
-			log('[ ✅ Design System Frontend JS ] ' + path.basename(file.path) + ' was created successfully.');
-		}))
-	}
+		// Design System Front End JS
+		if (designSystemJS.length){
+			let js = src(designSystemJS);
+
+			if (min) {
+				js = js.pipe(uglify());
+			}
+
+			js.pipe(lineec()) // Consistent Line Endings for non UNIX systems.
+			.pipe(concat(fileName)) // compiled file
+			.pipe(dest('build/js/'))
+			.pipe(tap(function (file) {
+				log('[ ✅ Design System Frontend ' + t + 'JS ] ' + path.basename(file.path) + ' was created successfully.');
+			}));		
+	
+		}
+	
+		fs.access('build/js/' + fileName, fs.F_OK, (err) => {
+			if (err) {
+				newFile(fileName, '')
+			        .pipe(dest('build/js/'));
+				return
+			}
+		  })
+	  } catch (error) {
+		// Note - error messages will vary depending on browser
+		console.error(fileName + ' failed to compile');
+	  }
 }
